0fe2974906273bf5072e1a380ac1dbe1e20e3dc6
GuillermoBaldan <guillermobaldan@gmail.com>
2022-04-04 05:23:22 +0200

    Moment 59:14

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-router.get("/signin", (req, res) => {
-  res.send("Ingresando a la app");
+router.get("/users/signin", (req, res) => {
+  res.render("users/signin");
 });
 
-router.get("/signup", (req, res) => {
-  res.send("Formulario de autenticación");
+router.get("/users/signup", (req, res) => {
+  res.render("users/signup");
 });
 
 module.exports = router;
