c9612545fd151cd849abe1cb4a48a6afafb2b058
GuillermoBaldan <guillermobaldan@gmail.com>
2022-04-04 06:18:44 +0200

    Moment 1:04

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-notes");
+});
+
 router.get("/notes", (req, res) => {
   res.send("Notes from database");
 });
