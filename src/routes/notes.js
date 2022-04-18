0d53e45dfc6c588aa5840a5f1e0ed1f218106add
GuillermoBaldan <guillermobaldan@gmail.com>
2022-04-18 17:46:15 +0200

    Moment 1:08:15

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -4,6 +4,26 @@ router.get("/notes/add", (req, res) => {
   res.render("notes/new-notes");
 });
 
+router.post("/notes/new-note", (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+  if (!title) {
+    errors.push({ text: "Please Write a title" });
+  }
+  if (!description) {
+    errors.push({ text: "Please Write a description" });
+  }
+  if (errors.length > 0) {
+    res.render("notes/new-notes", {
+      errors,
+      title,
+      description,
+    });
+  } else {
+    res.send("ok ");
+  }
+});
+
 router.get("/notes", (req, res) => {
   res.send("Notes from database");
 });
