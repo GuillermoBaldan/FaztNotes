423dc8ad168b6c873798c2afea04a6fce2d97e01
GuillermoBaldan <guillermobaldan@gmail.com>
2022-04-26 12:13:19 +0200

    Moment 1:37:03

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -29,7 +29,7 @@ router.post("/notes/new-note", async (req, res) => {
 });
 
 router.get("/notes", async (req, res) => {
-  const notes = await Note.find().lean();
+  const notes = await Note.find().sort({ date: "desc" }).lean();
   res.render("notes/all-notes", { notes });
 });
 
