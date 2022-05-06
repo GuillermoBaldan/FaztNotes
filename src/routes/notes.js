c54a953685d02a72f4f46edc5401371ecd73fc2b
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-06 21:25:04 +0200

    messages for different REST operantios

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -42,11 +42,13 @@ router.get("/notes/edit/:id", async (req, res) => {
 router.put("/notes/edit-note/:id", async (req, res) => {
   const { title, description } = req.body;
   await Note.findByIdAndUpdate(req.params.id, { title, description });
+  req.flash("success_msg", "Note Updated Successfully");
   res.redirect("/notes");
 });
 
 router.delete("/notes/delete/:id", async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
+  req.flash("success_msg", "Note Deleted Successfully");
   res.redirect("/notes");
 });
 
