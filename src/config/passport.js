eed8d5db0ae94423b33361ad45193ce6cbc9931e
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-07 23:50:44 +0200

    Moment 2:51:54 - End of Login/Sign In process

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,18 +1,33 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-const User = require('../models/User');
+const User = require("../models/User");
 
-passport.use(new LocalStrategy(
-    { usernameField: 'email' },
-   async (email, password, done) => {
-await user = await User.findOne({ email: email });
-if (!user) {
-    return done(null, false, { message: 'Not user found' });
-} else {
-    await user.matchPassword(password);
-}
-}
-));
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        return done(null, false, { message: "Not user found" });
+      } else {
+        const match = await user.matchPassword(password);
+        if (match) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      }
+    }
+  )
+);
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
+passport.deserializeUser(async (id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
